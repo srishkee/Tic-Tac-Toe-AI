71deb60ee9cb6816195aff0d5b0c8c84e3032a65
srishkee <skumar55@tamu.edu>
2019-12-22 13:44:19 -0600

    Added GUI for game level

--- src/com/srishti/tictactoe/view/View.java
+++ src/com/srishti/tictactoe/view/View.java
@@ -1,5 +1,6 @@
 package com.srishti.tictactoe.view;
 
+import com.intellij.openapi.ui.ComboBox;
 import com.srishti.tictactoe.model.LevelEnum;
 import com.srishti.tictactoe.model.Move;
 import com.srishti.tictactoe.model.PlayerEnum;
@@ -10,7 +11,7 @@ import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Scanner;
+import java.util.concurrent.Semaphore;
 
 public class View extends JFrame implements ActionListener
 {
@@ -18,20 +19,22 @@ public class View extends JFrame implements ActionListener
     private JButton buttons[][];
     private ImageIcon X, O;
     private JFrame msgFrame;
+    private JFrame userFrame;
     private int boardSize;
     private TicTacToeController controller;
-    private Scanner scanner;
+    private Semaphore semaphore;
+    private JComboBox comboBox;
 
 
     public View(int aBoardSize, TicTacToeController controller)
     {
         this.boardSize = aBoardSize;
         this.controller = controller;
-        this.scanner = new Scanner(System.in);
+        semaphore = new Semaphore(0); // Used to sync 1st and 2nd GUI window
     }
 
     public void actionPerformed(ActionEvent e) {
-
+        System.out.println(e.getActionCommand());
         int row = -1; int col = -1;
 
         // User move
@@ -44,7 +47,7 @@ public class View extends JFrame implements ActionListener
             }
         }
         if(!controller.isLegalMove(new Move(row, col))) return; // Don't allow illegal move
-        controller.placeMoveOnBoard( PlayerEnum.USER, new Move(row, col)); // GameState2 uses Internal board
+        controller.placeMoveOnBoard( PlayerEnum.USER, new Move(row, col)); // GameState uses Internal board
         buttons[row][col].setIcon(X); // Place move on GUI
         String gameState = controller.getGameState();
         if(gameState != null) {processTerminalState(gameState); return;}
@@ -52,7 +55,7 @@ public class View extends JFrame implements ActionListener
         // Computer move
         Move m = controller.getNextMove();
         row = m.x; col = m.y; // Get coordinates (always legal)
-        controller.placeMoveOnBoard(PlayerEnum.COMPUTER, new Move(row, col)); // GameState2 uses internal board
+        controller.placeMoveOnBoard(PlayerEnum.COMPUTER, new Move(row, col)); // GameState uses internal board
         buttons[row][col].setIcon(O); // Place move on GUI
         gameState = controller.getGameState();
         if(gameState != null) processTerminalState(gameState);
@@ -61,11 +64,17 @@ public class View extends JFrame implements ActionListener
     public void initGUI() {
 
         getUserInput();
+        try { // Only proceed once semaphore has been acquired (after game level has been selected)
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         p = new JPanel();
         X = new ImageIcon(this.getClass().getResource("../../../../XX.png"));
         O = new ImageIcon(this.getClass().getResource("../../../../OO.png"));
         setSize(800, 800);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        userFrame.setVisible(false); // Remove user window
         setVisible(true);
         setResizable(false);
         p.setLayout(new GridLayout(boardSize, boardSize));
@@ -75,12 +84,40 @@ public class View extends JFrame implements ActionListener
         setVisible(true);
     }
 
-    private void getUserInput()
+    public void getUserInput()
     {
-        System.out.println("Welcome to the Classic game of Tic-Tac-Toe!");
-        System.out.println("Choose game level: 1 for Easy, 2 for Medium, 3 for Hard");
-        int level = Integer.parseInt(scanner.nextLine());
-        controller.setLevel(LevelEnum.parseFromInt(level));
+        // Panel
+        JPanel p = new JPanel();
+        p.setLayout(new GridBagLayout());
+        GridBagConstraints gridConstraints = new GridBagConstraints();
+
+        // JLabel
+        JLabel levelLabel = new JLabel("Select game level: ");
+        levelLabel.setFont(new Font("Verdana", 1, 16));
+        String levels[] = {"Select Level", "Easy", "Medium", "Hard"};
+
+        // ComboBox
+        comboBox = new ComboBox(levels);
+        comboBox.setFont(new Font("Verdana", 1, 16));
+        comboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int level = comboBox.getSelectedIndex();
+                controller.setLevel(LevelEnum.parseFromInt(level));
+                semaphore.release(); // Program can only continue once game level has been selected
+            }
+        });
+        p.add(levelLabel, gridConstraints);
+        p.add(comboBox);
+
+        // JFrame
+        userFrame = new JFrame();
+        userFrame.setLayout(new GridBagLayout());
+        userFrame.add(p, gridConstraints);
+        userFrame.setSize(400, 260);
+        userFrame.setLocation(200, 290);
+        userFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        userFrame.setVisible(true);
     }
 
     private void initButtons() {
